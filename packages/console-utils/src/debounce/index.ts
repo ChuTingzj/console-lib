cccf4e594dd425ac805cbbc1a398313bf8899434
zhangjing <zhangjing@qianjiangcloud.com>
2023-02-17 09:54:43 +0800

    feature: @console/utils

---
+++ packages/console-utils/src/debounce/index.ts
@@ -0,0 +1,11 @@
+export function debounce(fn: () => void, delay = 500) {
+  let timer: NodeJS.Timeout
+  return function (this: any, ...args: any) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, args) // 改变this指向为调用debounce所指的对象
+    }, delay)
+  }
+}
---
+++ packages/console-utils/webpack.config.ts
@@ -0,0 +1,45 @@
+import { resolve } from 'path'
+import * as webpack from 'webpack'
+module.exports = () => {
+  return {
+    target: 'node',
+    mode: 'production',
+    entry: resolve(__dirname, './index.ts'),
+    output: {
+      publicPath: '/',
+      clean: true,
+      path: resolve(__dirname, './dist'),
+      filename: 'index.js',
+      library: {
+        name: '@console/utils',
+        type: 'umd'
+      }
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src')
+      },
+      extensions: ['.ts', '.d.ts', '...']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-typescript'],
+                plugins: ["@babel/plugin-transform-typescript"]
+              }
+            },
+            {
+              loader: 'ts-loader',
+            }
+          ]
+        },
+      ]
+    }
+  } satisfies webpack.Configuration
+}
