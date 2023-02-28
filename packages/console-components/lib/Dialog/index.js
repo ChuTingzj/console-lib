2fa4161ce021690dbe15d19cdbc26e06b586f35e
zhangjing <zhangjing@qianjiangcloud.com>
2023-02-28 11:32:26 +0800

    fix: version

--- packages/console-components/lib/Dialog/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Dialog = void 0;
-const jsx_runtime_1 = require("react/jsx-runtime");
-const Dialog = () => {
-    return (0, jsx_runtime_1.jsx)("div", { className: "", children: "index" });
-};
-exports.Dialog = Dialog;
